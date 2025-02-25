1cff1e4ac6fc6903b968d26338757737c0cd9a55
Mostafa Gholami <mostafagholamidev@gmail.com>
2025-02-25 10:55:52 +0330

    migrations updated
    Signed-off-by: Mostafa Gholami <mostafagholamidev@gmail.com>

---
+++ prisma/migrations/20250225072542_init/migration.sql
@@ -0,0 +1,99 @@
+-- CreateTable
+CREATE TABLE "locations" (
+    "id" VARCHAR(64) NOT NULL,
+    "city" TEXT,
+    "state" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "locations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "salaries" (
+    "id" VARCHAR(64) NOT NULL,
+    "min" INTEGER,
+    "max" INTEGER,
+    "currency" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "salaries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" VARCHAR(64) NOT NULL,
+    "name" TEXT NOT NULL,
+    "website" TEXT,
+    "industry" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "skills" (
+    "id" VARCHAR(64) NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "skills_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "jobs" (
+    "id" VARCHAR(64) NOT NULL,
+    "code" TEXT NOT NULL,
+    "company_id" VARCHAR(64) NOT NULL,
+    "salary_id" VARCHAR(64),
+    "location_id" VARCHAR(64),
+    "provider" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "type" VARCHAR(128),
+    "experience" INTEGER,
+    "remotely" BOOLEAN DEFAULT false,
+    "posted_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "jobs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_job_skill" (
+    "A" VARCHAR(64) NOT NULL,
+    "B" VARCHAR(64) NOT NULL,
+
+    CONSTRAINT "_job_skill_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "locations_city_state_key" ON "locations"("city", "state");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_name_key" ON "companies"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "skills_name_key" ON "skills"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "jobs_code_key" ON "jobs"("code");
+
+-- CreateIndex
+CREATE INDEX "jobs_type_remotely_posted_at_idx" ON "jobs"("type", "remotely", "posted_at");
+
+-- CreateIndex
+CREATE INDEX "_job_skill_B_index" ON "_job_skill"("B");
+
+-- AddForeignKey
+ALTER TABLE "jobs" ADD CONSTRAINT "jobs_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "jobs" ADD CONSTRAINT "jobs_salary_id_fkey" FOREIGN KEY ("salary_id") REFERENCES "salaries"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "jobs" ADD CONSTRAINT "jobs_location_id_fkey" FOREIGN KEY ("location_id") REFERENCES "locations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_job_skill" ADD CONSTRAINT "_job_skill_A_fkey" FOREIGN KEY ("A") REFERENCES "jobs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_job_skill" ADD CONSTRAINT "_job_skill_B_fkey" FOREIGN KEY ("B") REFERENCES "skills"("id") ON DELETE CASCADE ON UPDATE CASCADE;
